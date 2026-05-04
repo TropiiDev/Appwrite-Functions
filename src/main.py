9125dc71c58a9a1107d1b5dd5e5b99d45fa3cc37
TropiiDev <colin@tropii.dev>
2026-05-03 21:13:05 -0400

    Thanks Copilot

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def main(context):
         row_id=file_id
       )
 
-      context.log(file['fileId'])
+      context.log(file.data)
 
   except AppwriteException as err:
     context.error("Could not list users: " + repr(err))
